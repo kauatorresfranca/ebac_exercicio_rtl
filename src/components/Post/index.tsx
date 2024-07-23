3376821861b58d77ee73844706540ecda317687d
kauatorresfranca <kauatorresfranca2@gmail.com>
2024-07-23 18:15:14 -0300

    fazendo a configuração dos testes

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,18 +1,19 @@
-import styles from './Post.module.css';
-
-import PostComments from '../PostComments';
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
+import PostComments from "../PostComments";
+import styles from "./Post.module.css";
 
 type Props = {
     children: ReactNode;
     imageUrl: string;
-}
+};
 
 const Post = ({ children, imageUrl }: Props) => (
     <div className={styles.post}>
-        <img className={styles['post-image']} src={imageUrl} />
-        <p className={styles['post-text']}> {children} </p>
-        <PostComments />
+    <img className={styles["post-image"]} src={imageUrl} alt="Post" />
+    <p className={styles["post-text"]}> {children} </p>
+    <div data-testid="post-comments-wrapper">
+    <PostComments />
+    </div>
     </div>
 );
 
